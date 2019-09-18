9af4252ec35b6dd8a09c2f6554ec6818a198dde9
Andy <github@sockbot.com>
2019-09-18 15:21:09 -0700

    start dashboard feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME_DEV'], password: ENV['ADMIN_PASSWORD_DEV']
 
   def show
+    @products = 
+    # @categories
   end
+
 end
