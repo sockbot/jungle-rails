1bc012822916f9eac3f9829f92161f8392bc759d
Andy <github@sockbot.com>
2019-09-19 13:48:56 -0700

    add users controller, has_secure_password and signup form

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # resource :users, only: [:new, :create]
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
