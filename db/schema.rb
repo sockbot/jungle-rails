81b4998a145098a6aa9e253866c5b83f7476a32c
Andy <github@sockbot.com>
2019-09-19 14:32:00 -0700

    fix navbar and current_user, remove name from model

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20190919203353) do
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
