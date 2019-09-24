5163d7706dca33f8158bfacf8e2d0aad5ebb3b05
Andy <github@sockbot.com>
2019-09-24 09:20:09 -0700

    change tests to submit complete Product intances

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,34 +3,56 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   
   subject { described_class.new }
-
+  
   describe 'Validations' do
     it 'is valid with valid attributes' do
+      @category = Category.new
       subject.name = "NOT SURE"
       subject.price = 1234
       subject.quantity = 3838
-      subject.category = Category.new
+      subject.category = @category
       expect(subject).to be_valid
+      expect(subject.errors.any?).to be false
     end
-
+    
     it 'is not valid without a name' do
+      @category = Category.new
       subject.name = nil
+      subject.price = 1234
+      subject.quantity = 3838
+      subject.category = @category
       expect(subject).to_not be_valid
+      expect(subject.errors.any?).to be true
+      puts subject.errors.full_messages
     end
 
     it 'is not valid without a price' do
+      @category = Category.new
+      subject.name = "NOT SURE"
       subject.price = nil
+      subject.quantity = 3838
+      subject.category = @category
       expect(subject).to_not be_valid
+      expect(subject.errors.any?).to be true
     end
-
+    
     it 'is not valid without a quantity' do
+      @category = Category.new
+      subject.name = "NOT SURE"
+      subject.price = 1234
       subject.quantity = nil
+      subject.category = @category
       expect(subject).to_not be_valid
+      expect(subject.errors.any?).to be true
     end
-
+    
     it 'is not valid without a category' do
+      subject.name = "NOT SURE"
+      subject.price = 1234
+      subject.quantity = 3838
       subject.category = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.any?).to be true
     end
   end
 
